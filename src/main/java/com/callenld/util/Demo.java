47204d756b715426b3ef0db0d8ea19b6f5904d00
callenld <527670557@qq.com>
2019-03-26 10:12:14 +0800

    demo

--- src/main/java/com/callenld/util/Demo.java
+++ src/main/java/com/callenld/util/Demo.java
@@ -4,14 +4,15 @@ package com.callenld.util;
  * @Author: Callenld
  * @Date: 19-3-25
  */
-public class test {
+public class Demo {
     public static void main(String[] args) {
-        String json = HttpUtil.builder()
+        String result = HttpUtil.builder()
                 .addParams("appid", "appId")
                 .addParams("secret", "secret")
                 .addParams("js_code", "code")
                 .addParams("grant_type", "authorization_code")
                 .doGet("https://api.weixin.qq.com/sns/jscode2session")
                 .toJson();
+        System.out.println(result);
     }
 }
